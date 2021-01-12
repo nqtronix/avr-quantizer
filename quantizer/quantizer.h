f633f91b6829689d710e369fcaf71f84c443d947
nqtronix <nqtronix@gmx.net>
2021-01-12 13:14:55 +0100

    Update LUTs for more precise binning boarders
    See explanation in quantizer.h file

--- quantizer/quantizer.h
+++ quantizer/quantizer.h
@@ -23,20 +23,33 @@
 // const __flash uint8_t lut16_E12_1K1M[49] =
 // {
 // 	// header
-// 	10, 22, 25, 29, 33, 36, 40, 44, 47, 48, 
+// 	10, 22, 25, 29, 33, 36, 40, 44, 47, 48,
 // 
 // 	// data
-// 	28, 34, 42, 52, 63, 77, 94, 113, 134, 161, 194, 234, 
-// 	142, 172, 211, 
-// 	129, 156, 191, 234, 
-// 	141, 168, 201, 242, 
-// 	146, 178, 215, 
-// 	132, 161, 195, 239, 
-// 	146, 176, 210, 251, 
-// 	151, 183, 222, 
-// 	134, 
+// 	27, 33, 41, 51, 62, 76, 93, 112, 133, 160, 193, 233,
+// 	141, 171, 210,
+// 	128, 155, 190, 233,
+// 	140, 167, 200, 241,
+// 	145, 177, 214,
+// 	131, 160, 194, 238,
+// 	145, 175, 209, 250,
+// 	150, 182, 221,
+// 	133,
 // 	255
-// // Explanation:// The first bytes are the header. It contains indices to the data section, each element in the// header corresponds to a row. The algorithm shifts the eg. 2 byte input data until the resulting// value is 255 or less so that it can be compared to the values in the LUT. For each shift, the// row counter is advanced by one.
+// };// Explanation:// The first bytes are the header. It contains indices to the data section, each element in the// header corresponds to a row. The algorithm shifts the eg. 2 byte input data until the resulting// value is 255 or less so that it can be compared to the values in the LUT. For each shift, the// row counter is advanced by one.//// The comparison values are rounded down upper limits and which can result in unexpected borders
+// between the bins. Let's take an example and assume a 8.8 fix point formating as an input value
+// which shall be mapped to 8, 10 or 12. You may choose the values in between (9, 11, 13) as borders
+// and get the following mapping;
+//	9:	0..9	-> 8
+//	11:	9..11	-> 10
+//	13:	11..13	-> 12
+// Assume now that you enter the binary equivalent to 11.5. According to the table above you'd assume
+// to get "2" as a result, matching the entry "12"; but it'll actually be "1". The reason is that
+// the shifting included in the algorithm inherently rounds down the input data (to 11 in this case)
+// and that passes the comparison "data <= 11 (lut entry)".
+// Changing the code will result in a less efficient implementation, so it is suggested to change
+// the LUT instead. The included excel files compensate for this already.
+
 
 //////////////////////////////////////////////////////////////////////////
 // Function Declarations
--- quantizer/quantizer_demo.c
+++ quantizer/quantizer_demo.c
@@ -17,57 +17,57 @@
 const __flash uint8_t lut16_E12_1K1M[49] =
 {
 	// header
-	10, 22, 25, 29, 33, 36, 40, 44, 47, 48, 
+	10, 22, 25, 29, 33, 36, 40, 44, 47, 48,
 
 	// data
-	28, 34, 42, 52, 63, 77, 94, 113, 134, 161, 194, 234, 
-	142, 172, 211, 
-	129, 156, 191, 234, 
-	141, 168, 201, 242, 
-	146, 178, 215, 
-	132, 161, 195, 239, 
-	146, 176, 210, 251, 
-	151, 183, 222, 
-	134, 
+	27, 33, 41, 51, 62, 76, 93, 112, 133, 160, 193, 233,
+	141, 171, 210,
+	128, 155, 190, 233,
+	140, 167, 200, 241,
+	145, 177, 214,
+	131, 160, 194, 238,
+	145, 175, 209, 250,
+	150, 182, 221,
+	133,
 	255
 };
 
 const __flash uint8_t lut32_E12_10R10M[93] =
 {
 	// header
-	18, 36, 39, 43, 47, 50, 54, 57, 61, 65, 68, 72, 76, 79, 83, 86, 90, 92, 
+	18, 36, 39, 43, 47, 50, 54, 57, 61, 65, 68, 72, 76, 79, 83, 86, 90, 92,
 
 	// data
-	9, 11, 14, 17, 20, 25, 30, 36, 43, 52, 62, 75, 91, 110, 135, 165, 200, 245, 
-	150, 180, 215, 
-	129, 155, 188, 228, 
-	138, 169, 206, 250, 
-	153, 188, 225, 
-	134, 161, 194, 234, 
-	142, 172, 211, 
-	129, 156, 191, 234, 
-	141, 168, 201, 242, 
-	146, 178, 215, 
-	132, 161, 195, 239, 
-	146, 176, 210, 251, 
-	151, 183, 222, 
-	134, 165, 201, 244, 
-	150, 183, 220, 
-	131, 157, 189, 229, 
-	139, 168, 
+	8, 10, 13, 16, 19, 24, 29, 35, 42, 51, 61, 74, 90, 109, 134, 164, 199, 244,
+	149, 179, 214,
+	128, 154, 187, 227,
+	137, 168, 205, 249,
+	152, 187, 224,
+	133, 160, 193, 233,
+	141, 171, 210,
+	128, 155, 190, 233,
+	140, 167, 200, 241,
+	145, 177, 214,
+	131, 160, 194, 238,
+	145, 175, 209, 250,
+	150, 182, 221,
+	133, 164, 200, 243,
+	149, 182, 219,
+	130, 156, 188, 228,
+	138, 167,
 	255
 };
 
 const __flash uint8_t lut16_E12_1K47K[28] =
 {
 	// header
-	5, 17, 20, 24, 27, 
+	5, 17, 20, 24, 27,
 
 	// data
-	28, 34, 42, 52, 63, 77, 94, 113, 134, 161, 194, 234, 
-	142, 172, 211, 
-	129, 156, 191, 234, 
-	141, 168, 201, 
+	27, 33, 41, 51, 62, 76, 93, 112, 133, 160, 193, 233,
+	141, 171, 210,
+	128, 155, 190, 233,
+	140, 167, 200,
 	255
 };
 
@@ -79,70 +79,70 @@ const __flash uint8_t lut16_E12_1K47K[28] =
 int main(void)
 {
     // test the upper and lower limit of each bin at start, middle and end of the LUT table 
-// 	volatile uint8_t id0L = quantizer_uint16(lut16_E12_1K1M, 0);
-// 	volatile uint8_t id0H = quantizer_uint16(lut16_E12_1K1M, 28);
-// 	volatile uint8_t id1L = quantizer_uint16(lut16_E12_1K1M, 29);
-// 	volatile uint8_t id1H = quantizer_uint16(lut16_E12_1K1M, 34);
-// 	volatile uint8_t id2L = quantizer_uint16(lut16_E12_1K1M, 35);
-// 	
-// 	volatile uint8_t id14H = quantizer_uint16(lut16_E12_1K1M, 211<<1);
-// 	volatile uint8_t id15L = quantizer_uint16(lut16_E12_1K1M, 212<<1);
-// 	volatile uint8_t id15H = quantizer_uint16(lut16_E12_1K1M, 129<<2);
-// 	volatile uint8_t id16L = quantizer_uint16(lut16_E12_1K1M, 130<<2);
-// 	
-// 	volatile uint8_t id36H = quantizer_uint16(lut16_E12_1K1M, 222<<7);
-// 	volatile uint8_t id37L = quantizer_uint16(lut16_E12_1K1M, 223<<7);
-// 	volatile uint8_t id37H = quantizer_uint16(lut16_E12_1K1M, 134<<8);
-// 	volatile uint8_t id38L = quantizer_uint16(lut16_E12_1K1M, 135<<8);
-// 	volatile uint8_t id38H = quantizer_uint16(lut16_E12_1K1M, UINT16_MAX);
+ 	volatile uint8_t id0L = quantizer_uint16(lut16_E12_1K1M, 0);
+	volatile uint8_t id0H = quantizer_uint16(lut16_E12_1K1M, 27);
+	volatile uint8_t id1L = quantizer_uint16(lut16_E12_1K1M, 28);
+	volatile uint8_t id1H = quantizer_uint16(lut16_E12_1K1M, 33);
+	volatile uint8_t id2L = quantizer_uint16(lut16_E12_1K1M, 34);
+	
+	volatile uint8_t id14H = quantizer_uint16(lut16_E12_1K1M, 210<<1);
+	volatile uint8_t id15L = quantizer_uint16(lut16_E12_1K1M, 211<<1);
+	volatile uint8_t id15H = quantizer_uint16(lut16_E12_1K1M, 128<<2);
+	volatile uint8_t id16L = quantizer_uint16(lut16_E12_1K1M, 129<<2);
+	
+	volatile uint8_t id36H = quantizer_uint16(lut16_E12_1K1M, 221<<7);
+	volatile uint8_t id37L = quantizer_uint16(lut16_E12_1K1M, 222<<7);
+	volatile uint8_t id37H = quantizer_uint16(lut16_E12_1K1M, 133<<8);
+	volatile uint8_t id38L = quantizer_uint16(lut16_E12_1K1M, 134<<8);
+	volatile uint8_t id38H = quantizer_uint16(lut16_E12_1K1M, UINT16_MAX);
 	
 	// test behavior id max input value is <32k => row count <9
 	// to be valid, this test requires a lut where: lut[lut[0]] < lut[lut[0]+1]
 	// output range is limited to the amount of elements, where ID23 is the max value
 // 	volatile uint8_t id0L = quantizer_uint16(lut16_E12_1K47K, 0);
-// 	volatile uint8_t id0H = quantizer_uint16(lut16_E12_1K47K, 28);
-// 	volatile uint8_t id1L = quantizer_uint16(lut16_E12_1K47K, 29);
-// 	volatile uint8_t id1H = quantizer_uint16(lut16_E12_1K47K, 34);
-// 	volatile uint8_t id2L = quantizer_uint16(lut16_E12_1K47K, 35);
+// 	volatile uint8_t id0H = quantizer_uint16(lut16_E12_1K47K, 27);
+// 	volatile uint8_t id1L = quantizer_uint16(lut16_E12_1K47K, 28);
+// 	volatile uint8_t id1H = quantizer_uint16(lut16_E12_1K47K, 33);
+// 	volatile uint8_t id2L = quantizer_uint16(lut16_E12_1K47K, 34);
 // 	
-// 	volatile uint8_t id14H = quantizer_uint16(lut16_E12_1K47K, 211<<1);
-// 	volatile uint8_t id15L = quantizer_uint16(lut16_E12_1K47K, 212<<1);
-// 	volatile uint8_t id15H = quantizer_uint16(lut16_E12_1K47K, 129<<2);
-// 	volatile uint8_t id16L = quantizer_uint16(lut16_E12_1K47K, 130<<2);
+// 	volatile uint8_t id14H = quantizer_uint16(lut16_E12_1K47K, 210<<1);
+// 	volatile uint8_t id15L = quantizer_uint16(lut16_E12_1K47K, 211<<1);
+// 	volatile uint8_t id15H = quantizer_uint16(lut16_E12_1K47K, 128<<2);
+// 	volatile uint8_t id16L = quantizer_uint16(lut16_E12_1K47K, 129<<2);
 // 	
-// 	volatile uint8_t id21H = quantizer_uint16(lut16_E12_1K47K, 201<<3);
-// 	volatile uint8_t id22L = quantizer_uint16(lut16_E12_1K47K, 202<<3);
+// 	volatile uint8_t id21H = quantizer_uint16(lut16_E12_1K47K, 200<<3);
+// 	volatile uint8_t id22L = quantizer_uint16(lut16_E12_1K47K, 201<<3);
 // 	volatile uint8_t id22H = quantizer_uint16(lut16_E12_1K47K, 254<<4);
 // 	volatile uint8_t id23L = quantizer_uint16(lut16_E12_1K47K, 255<<4);
 // 	volatile uint8_t id23H = quantizer_uint16(lut16_E12_1K47K, UINT16_MAX);
 	
 	// test uint8 variant
 // 	volatile uint8_t id0L = quantizer_uint8(lut16_E12_1K47K, 0);
-// 	volatile uint8_t id0H = quantizer_uint8(lut16_E12_1K47K, 28);
-// 	volatile uint8_t id1L = quantizer_uint8(lut16_E12_1K47K, 29);
-// 	volatile uint8_t id1H = quantizer_uint8(lut16_E12_1K47K, 34);
-// 	volatile uint8_t id2L = quantizer_uint8(lut16_E12_1K47K, 35);
+// 	volatile uint8_t id0H = quantizer_uint8(lut16_E12_1K47K, 27);
+// 	volatile uint8_t id1L = quantizer_uint8(lut16_E12_1K47K, 28);
+// 	volatile uint8_t id1H = quantizer_uint8(lut16_E12_1K47K, 33);
+// 	volatile uint8_t id2L = quantizer_uint8(lut16_E12_1K47K, 34);
 // 
-// 	volatile uint8_t id11H = quantizer_uint8(lut16_E12_1K47K, 234);
-// 	volatile uint8_t id12L = quantizer_uint8(lut16_E12_1K47K, 235);
+// 	volatile uint8_t id11H = quantizer_uint8(lut16_E12_1K47K, 233);
+// 	volatile uint8_t id12L = quantizer_uint8(lut16_E12_1K47K, 234);
 // 	volatile uint8_t id12H = quantizer_uint8(lut16_E12_1K47K, 255);
 
 	// test uint32 variant
-// 	volatile uint8_t id0L = quantizer_uint32(lut32_E12_10R10M, 0);
-// 	volatile uint8_t id0H = quantizer_uint32(lut32_E12_10R10M, 9);
-// 	volatile uint8_t id1L = quantizer_uint32(lut32_E12_10R10M, 10);
-// 	volatile uint8_t id1H = quantizer_uint32(lut32_E12_10R10M, 11);
-// 	volatile uint8_t id2L = quantizer_uint32(lut32_E12_10R10M, 12);
+//	volatile uint8_t id0L = quantizer_uint32(lut32_E12_10R10M, 0);
+// 	volatile uint8_t id0H = quantizer_uint32(lut32_E12_10R10M, 8);
+// 	volatile uint8_t id1L = quantizer_uint32(lut32_E12_10R10M, 9);
+// 	volatile uint8_t id1H = quantizer_uint32(lut32_E12_10R10M, 10);
+// 	volatile uint8_t id2L = quantizer_uint32(lut32_E12_10R10M, 11);
 // 
-// 	volatile uint8_t id20H = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)215)<<1);
-// 	volatile uint8_t id21L = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)216)<<1);
-// 	volatile uint8_t id21H = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)129)<<2);
-// 	volatile uint8_t id22L = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)130)<<2);
+// 	volatile uint8_t id20H = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)214)<<1);
+// 	volatile uint8_t id21L = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)215)<<1);
+// 	volatile uint8_t id21H = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)128)<<2);
+// 	volatile uint8_t id22L = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)129)<<2);
 // 
-// 	volatile uint8_t id72H = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)139)<<16);
-// 	volatile uint8_t id73L = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)140)<<16);
-// 	volatile uint8_t id73H = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)168)<<16);
-// 	volatile uint8_t id74L = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)169)<<16);
+// 	volatile uint8_t id72H = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)128)<<16);
+// 	volatile uint8_t id73L = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)139)<<16);
+// 	volatile uint8_t id73H = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)167)<<16);
+// 	volatile uint8_t id74L = quantizer_uint32(lut32_E12_10R10M, ((uint32_t)168)<<16);
 // 	volatile uint8_t id74H = quantizer_uint32(lut32_E12_10R10M, UINT32_MAX);
 	
 	// test average execution time 
